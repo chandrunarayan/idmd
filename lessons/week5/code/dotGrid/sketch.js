e3280f512824f833e32eda36c44383af507dd403
chandrunarayan <54154611+chandrunarayan@users.noreply.github.com>
2021-02-21 19:18:22 -0800

    Update sketch.js

--- lessons/week5/code/dotGrid/sketch.js
+++ lessons/week5/code/dotGrid/sketch.js
@@ -1,11 +1,20 @@
 function setup() {
+  let dia = 10;
   createCanvas(400, 400);
-}
-
-function draw() {
-  for(y = 20; y < height - 20; y = y + 5){
-    for(x = 20; x < width - 20; x = x + 5){
-      point(x,y);  
+  background(200);
+  strokeWeight(1);
+  for (y = 40; y < height; y = y + 40) {
+    for (x = 40; x < width; x = x + 40) {
+      let rc = randomColor()
+      stroke(rc);
+      fill(rc);
+      ellipse(x, y, 10, 10);
+      line(x, y, width / 2, height / 2);
     }
   }
+
+}
+
+function randomColor() {
+  return color(random(255), random(255), random(255));
 }
