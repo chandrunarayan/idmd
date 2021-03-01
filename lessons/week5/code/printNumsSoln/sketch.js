1ddb22e7d9d9057a56914efead67f62991bf3130
chandrunarayan <54154611+chandrunarayan@users.noreply.github.com>
2021-03-01 15:28:51 -0800

    Update sketch.js

--- lessons/week5/code/printNumsSoln/sketch.js
+++ lessons/week5/code/printNumsSoln/sketch.js
@@ -1,5 +1,5 @@
 // col wise reversed operation
-var nums = [1, 2, 3, 4, 7, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
+var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 let cols = 10; // number of cols not pixels
 let rows = 5; // number of rows
 let count = 0;
